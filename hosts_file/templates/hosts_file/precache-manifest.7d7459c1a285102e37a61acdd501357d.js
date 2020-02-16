83b736cd520b76691540391425a05b5d8f4258c3
Jdrzej Kucharczyk <jkucharczyk@protonmail.com>
2020-02-17 00:50:59 +0100

    React TodoList oraz dodany React do django - nalezy wykonac\n - npm run build\n - skopiowac foldery z folderu build/static do folderu hosts_file/static oraz skopiowac reszte zawartosci folderu do hosts_file/templates/hosts_file

---
+++ hosts_file/templates/hosts_file/precache-manifest.7d7459c1a285102e37a61acdd501357d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce3b0d25f345086e13426105fe169e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6b7e6f334cd3482526",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f42efe15520d3f2b8dab",
+    "url": "/static/js/2.dc77f32a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.dc77f32a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b7e6f334cd3482526",
+    "url": "/static/js/main.6a98fab5.chunk.js"
+  },
+  {
+    "revision": "e18889eed4618336758b",
+    "url": "/static/js/runtime-main.1dee3fc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
